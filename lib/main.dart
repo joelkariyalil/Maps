71c11d36f24bb3bced44da683a9b9141931b8ee6
joelkariyalil1 <joeltc071@gmail.com>
2023-03-07 22:53:10 +0530

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:latlong/latlong.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(key: UniqueKey(),title: 'Flutter Demo Home Page'),
+      home: MapApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-
-  final Key key;
-  final String title;
-  MyHomePage({required this.key, required this.title}) : super(key: key);
-
+class MapApp extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MapAppState createState() => _MapAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _message = '';
-
-  void _setMessage() {
-    setState(() {
-      _message = 'Hello! World!';
-    });
-  }
+class _MapAppState extends State<MapApp> {
+  double long = 49.5;
+  double lat = -0.09;
+  LatLng point = LatLng(49.5, -0.09);
+  var location = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _message,
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _setMessage,
-              child: Text('Press me'),
+    return Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+            onTap: (p) async {
+              location = await Geocoder.local.findAddressesFromCoordinates(
+                  new Coordinates(p.latitude, p.longitude));
+
+              setState(() {
+                point = p;
+                print(p);
+              });
+
+              print(
+                  "${location.first.countryName} - ${location.first.featureName}");
+            },
+            center: LatLng(49.5, -0.09),
+            zoom: 5.0,
+          ),
+          layers: [
+            TileLayerOptions(
+                urlTemplate:
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c']),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: point,
+                  builder: (ctx) => Container(
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                    ),
+                  ),
+                )
+              ],
             ),
           ],
         ),
-      ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 34.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Card(
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(16.0),
+                    hintText: "Search for your localisation",
+                    prefixIcon: Icon(Icons.location_on_outlined),
+                  ),
+                ),
+              ),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                          "${location.first.countryName},${location.first.locality}, ${location.first.featureName}"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
